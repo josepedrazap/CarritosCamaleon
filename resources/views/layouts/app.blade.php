163781589cb87c26ed899d0f72732d055a4d2504
jota <jotonpedraza@gmail.com>
2018-03-22 21:28:34 -0300

    cambios 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,9 +42,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            
+                        @guest                            
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
