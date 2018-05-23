c91be71493cf403f56eca046684d0aa76cf60f34
jota <jotonpedraza@gmail.com>
2018-05-23 16:15:33 -0400

    ver editar comprobantes

--- app/Http/Controllers/ComprasController.php
+++ app/Http/Controllers/ComprasController.php
@@ -26,8 +26,8 @@ class ComprasController extends Controller
         ->where('tipo_dato', '=', 'compra')
         ->join('proveedores as prov', 'prov.id', '=', 'df.id_tercero')
         ->whereBetween('fecha_ingreso', array($date_1, $date_2))
-        ->select('df.id', 'tipo_documento', 'excento', 'otros_impuestos','df.fecha_documento','numero_documento', 'monto_neto', 'iva', 'total', 'rut')
-        ->groupBy('df.id', 'tipo_documento', 'excento', 'otros_impuestos', 'fecha_documento','numero_documento', 'monto_neto', 'iva', 'total', 'rut')
+        ->select('df.id', 'df.numero_comprobante','tipo_documento', 'excento', 'otros_impuestos','df.fecha_documento','numero_documento', 'monto_neto', 'iva', 'total', 'rut')
+        ->groupBy('df.id', 'df.numero_comprobante','tipo_documento', 'excento', 'otros_impuestos', 'fecha_documento','numero_documento', 'monto_neto', 'iva', 'total', 'rut')
         ->orderBy('id','desc')
         ->get();
         return View('carritos.compras.index', ["facturas"=>$facturas, "date_1"=>$date_1, "date_2"=>$date_2]);
