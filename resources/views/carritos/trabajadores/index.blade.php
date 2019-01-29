cd339a4a8c19c9157c27d860ede7a99b550c19ca
jota <jotonpedraza@gmail.com>
2019-01-29 19:15:51 -0300

    nuevo

--- resources/views/carritos/trabajadores/index.blade.php
+++ resources/views/carritos/trabajadores/index.blade.php
@@ -25,7 +25,7 @@
             @if($tra->telefono[1] == 5 && $tra->telefono[2] == 6 && $tra->telefono[3] == 9 && strlen($tra->telefono) >= 11)
               <td>
                 <a href="{{"https://api.whatsapp.com/send?phone=$tra->telefono"}}" target="_blank">
-                  <IMG SRC="{{ asset('img/img_wh.png') }}" WIDTH=20 HEIGHT=20>    {{$tra->telefono}}
+                  <IMG SRC="{{ asset('img/img_wh.png') }}" WIDTH=20 HEIGHT=20>    {{substr($tra->telefono, 1)}}
                 </a>
               </td>
             @else
